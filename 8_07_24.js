8468f32fa0a5a03a87657517b6f0ac9299dea878
Md. Arafat Rahman Rana <aranab.dba@gmail.com>
2024-07-10 00:28:36 +0600

    Added few keywords.

---
+++ 8_07_24.js
@@ -0,0 +1,25 @@
+// JavaScript version: ES1-ES5; ES6-ESnext
+
+// 16GB - 16*8=128GBit
+
+// Memory:
+// Read - read
+// Write - write, edit/update, delete
+// Boundary - Module
+
+// Data Type and Data Structure
+
+// Stack
+// Queue
+// List
+// Array
+// Tree: parent-child
+// Node: no parent-child
+
+// Loss Coupling; High Cohesion;
+
+// Assignment statement
+// conditional statement
+// loop
+
+
