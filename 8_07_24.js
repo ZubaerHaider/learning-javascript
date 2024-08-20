532677aa19c0ebf74d6480366719c8fe9dcd965a
Md. Arafat Rahman Rana <aranab.dba@gmail.com>
2024-08-20 22:11:41 +0600

    Understanding task, behavior and functionality.

--- 8_07_24.js
+++ 8_07_24.js
@@ -22,4 +22,53 @@
 // conditional statement
 // loop
 
+// Principle: persistence ignorance.
+// Data centric. [ignorance]
+// Behavior/functionality centric. [purity]
+// C programming language
+// Multi-paradigm(OOP + Functional programming): C++, Java, C#, python, JavaScript
 
+// O-Object
+
+// coined the term: Alan Kay (biologist), Smalltalk (pure oop)
+
+// Class: big function, small function(pure)
+
+// Command/request(language)(modeling)(language paradigm) -> ordering(forcefully), requesting, don't know how to do it.
+
+// What is model: a structure or a way of solution of a problem.
+
+// Paradigm is a model.
+
+// Language paradigm:
+// - Declarative [ordering(forcefully)]
+//   - Object-oriented (don't know how to do it.)
+// - Imperative  [requesting]
+//   - Functional (don't know how to do it.)
+
+// JavaScript (after 2015 , ES6) is published as multi-paradigm language.
+
+// Tasks: apple counting.
+// 1. filtering good and bad.
+// 2. filtering colors.
+// 3. filtering sizes.
+
+// you should develop logical behave -> strength or capability or capacity of doing a some tasks.
+
+// programming: behavior + task = functionality.
+
+// functional programming (small function): one function represents one single behavior.
+// objection oriented programming(class: big function): one function represents one single task, inside this one function will represent more small functions.
+
+// Modularization
+//import sumFun from "./11_08_24.js";
+
+// Tasks: summation two numbers.
+
+export default function Sum(a, b) { // functional programming
+  return a - b; // subtraction
+}
+
+let result = Sum(5, 4); // = 9
+
+console.log(result);
